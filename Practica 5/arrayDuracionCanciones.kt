73defa37a701e002db13068062d22cc39eeeed77
Diego Alessandro Alvarez Cruz <116934048+Alessandro-22@users.noreply.github.com>
2024-09-30 18:01:32 -0500

    se creo un array para almacenar el tiempo de las canciones

---
+++ Practica 5/arrayDuracionCanciones.kt
@@ -0,0 +1,13 @@
+// Nuevo array con las duraciones de las canciones en formato "min:seg"
+    private val songDurations = arrayOf(
+        "2:45",
+        "3:10",
+        "3:36",
+        "2:58",
+        "3:30",
+        "4:05",
+        "3:52",
+        "3:20",
+        "3:40",
+        "4:30"
+    )
